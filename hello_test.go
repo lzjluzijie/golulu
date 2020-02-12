60284ea533619cda48b562f8c89baf82f51202ae
Halulu <lzjluzijie@gmail.com>
2020-02-11 23:42:04 -0500

    我回来啦

---
+++ hello_test.go
@@ -0,0 +1,10 @@
+package golulu
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	want := "Hello, world."
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
